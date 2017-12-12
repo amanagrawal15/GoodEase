28055b18497dc79c2415c64875b3ea73005fc5e8
Aman Agrawal <amanagrawal15@gmail.com>
2017-12-11 22:22:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hp/goodease/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.example.hp.goodease;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SecondActivity extends AppCompatActivity {
+private Button Sign_out;
+    private TextView welcome;
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        welcome =(TextView)findViewById(R.id.tvwelcome);
+        Sign_out = (Button)findViewById(R.id.btnsign_out);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        welcome.setText("WELCOME "+user.getEmail());
+        Sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(SecondActivity.this, Login.class));
+            }
+        });
+    }
+
+}

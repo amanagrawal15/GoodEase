782358b1a3b97232b2747cf57222b4b969ad0aa1
Aman Agrawal <amanagrawal15@gmail.com>
2017-12-12 18:38:31 -0300

    dropdownsize commit

--- app/src/main/java/com/example/hp/goodease/SecondActivity.java
+++ app/src/main/java/com/example/hp/goodease/SecondActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,18 +16,21 @@ import com.google.firebase.auth.FirebaseUser;
 public class SecondActivity extends AppCompatActivity {
 private Button Sign_out;
     private TextView welcome;
-
+    private Spinner ddsize;
     private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         welcome =(TextView)findViewById(R.id.tvwelcome);
-
+        ddsize = (Spinner)findViewById(R.id.sp_size);
         Sign_out = (Button)findViewById(R.id.btnsign_out);
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
         welcome.setText("WELCOME "+user.getEmail());
+        ArrayAdapter<String> myAdapter= new ArrayAdapter<String>(SecondActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.size));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ddsize.setAdapter(myAdapter);
         Sign_out.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
